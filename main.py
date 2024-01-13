6793909f31fde491c282826e46eaa786aea4060e
MohamadReza Karimi <mokar2001@gmail.com>
2024-01-13 09:45:25 +0000

    🥸 Add Frechet Distance

--- main.py
+++ main.py
@@ -3,30 +3,42 @@ import torch
 from torchvision import models
 from utils.load import Loader
 from metrics.quality.inception import calculate_inception_score
+from metrics.quality.frechet import calculate_frechet_inception_distance
 from torchvision.models import Inception_V3_Weights
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 
-def inception_handler(path):
+def inception_handler(gpath):
     weights = Inception_V3_Weights.IMAGENET1K_V1 
     inception_model = models.inception_v3(weights=weights).to(DEVICE)
-    generated_dataset = Loader.load(path, 32)
+    generated_dataset = Loader.load(gpath, 32)
     mean_is, std_is = calculate_inception_score(generated_dataset, inception_model)
     print(f"Inception Score: {mean_is}, Standard Deviation: {std_is}")
 
 
+def frechet_handler(gpath, rpath):
+    weights = Inception_V3_Weights.IMAGENET1K_V1 
+    inception_model = models.inception_v3(weights=weights).to(DEVICE)
+    generated_dataset = Loader.load(gpath, 32)
+    real_dataset = Loader.load(rpath, 32)
+    fid_score = calculate_frechet_inception_distance(real_dataset, generated_dataset, inception_model)
+    print(f"Frechet Inception Distance: {fid_score}")
 
-def main(space, task, path):
+
+def main(space, task, gpath, rpath):
     if space == 'metric':
         if task == 'inception':
-            inception_handler(path)
+            inception_handler(gpath)
+        elif task == 'frechet':
+            frechet_handler(gpath, rpath)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Sharif ML-Lab Data Generation ToolKit")
     parser.add_argument('-s', '--space', type=str, required=True, help="Space Name (e.g. metric, crawl)")
     parser.add_argument('-t', '--task', type=str, required=True, help="Task Name (e.g. inception, knn)")
-    parser.add_argument('-p', '--path', type=str, required=True, help="Generated Data Path")
+    parser.add_argument('-gp', '--gpath', type=str, required=True, help="Generated Data Path")
+    parser.add_argument('-rp', '--rpath', type=str, required=False, help="Real Data Path")
 
     args = parser.parse_args()
-    main(args.space, args.task, args.path)
+    main(args.space, args.task, args.gpath, args.rpath)
---
+++ metrics/quality/frechet.py
@@ -0,0 +1,39 @@
+import torch
+import numpy as np
+from scipy import linalg
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+def calculate_frechet_inception_distance(loader_real, loader_fake, inception_model):
+    inception_model.eval()
+
+    real_features = []
+    fake_features = []
+
+    with torch.no_grad():
+        for real_images in loader_real:
+            real_images = real_images.to(DEVICE)
+            real_pred = inception_model(real_images)
+            real_features.append(real_pred)
+
+        for fake_images in loader_fake:
+            fake_images = fake_images.to(DEVICE)
+            fake_pred = inception_model(fake_images)
+            fake_features.append(fake_pred)
+
+    real_features = torch.cat(real_features, 0).cpu().numpy()
+    fake_features = torch.cat(fake_features, 0).cpu().numpy()
+
+    mu_real, sigma_real = real_features.mean(axis=0), np.cov(real_features, rowvar=False)
+    mu_fake, sigma_fake = fake_features.mean(axis=0), np.cov(fake_features, rowvar=False)
+
+    ssdiff = np.sum((mu_real - mu_fake) ** 2.0)
+
+    covmean = linalg.sqrtm(sigma_real.dot(sigma_fake))
+
+    if np.iscomplexobj(covmean):
+        covmean = covmean.real
+
+    fid = ssdiff + np.trace(sigma_real + sigma_fake - 2.0 * covmean)
+
+    return fid
