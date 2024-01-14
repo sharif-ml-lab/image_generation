852dff74190ae2690301420709208ab40a04595b
MohamadReza Karimi <mokar2001@gmail.com>
2024-01-14 13:10:35 +0000

    🔥 Add Canny Edge Density Function

--- metrics/quality/realism.py
+++ metrics/quality/realism.py
@@ -2,9 +2,10 @@ import cv2
 import numpy as np
 from skimage.feature import greycomatrix, greycoprops
 
+
 def compute_glcm(image_path):
     image = cv2.imread(image_path, 0)
-    image = cv2.resize(image, (100, 100))  
+    image = cv2.resize(image, (100, 100))  # HyperParameter
     glcm = greycomatrix(image, distances=[1], angles=[0, np.pi/4, np.pi/2, 3*np.pi/4], levels=256, symmetric=True, normed=True)
     contrast = greycoprops(glcm, 'contrast')
     dissimilarity = greycoprops(glcm, 'dissimilarity')
@@ -19,3 +20,12 @@ def compute_glcm(image_path):
         'energy': energy,
         'correlation': correlation
     }
+
+
+def compute_canny_edge_density(image_path):
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    edges = cv2.Canny(image, 100, 200) # HyperParameter
+    edge_pixels = np.sum(edges > 0)
+    total_pixels = image.shape[0] * image.shape[1]
+    edge_density = edge_pixels / total_pixels
+    return edge_density
